a8cb9b8615d8271f79fef145d39c778c7c44e99e
Romigit <romigit@users.noreply.github.com>
2020-04-02 03:36:12 -0300

    second  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'test2';
+  vAnimal : string;
+  srcAnimal : string;
+  
+  constructor(){}
+  
+  ngOnInit(){
+    this.vAnimal="perritos";
+    this.srcAnimal="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9ZcQmJhRcU4LAuJ13ib5ycbB3mEPIP0osz5o1iKSlEtsE2HLY&usqp=CAU";
+  }
+
+  public enviar(event): void{
+    this.vAnimal="Pipo";
+  }
+
 }
---
+++ src/app/componentes/bindeo/bindeo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bindeo',
+  templateUrl: './bindeo.component.html',
+  styleUrls: ['./bindeo.component.css']
+})
+export class BindeoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
